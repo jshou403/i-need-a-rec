f2bfa6666d2865864959203c8e828d2a668b331e
JACK_E <jshou403@gmail.com>
2020-01-31 14:44:25 -0500

    Forked repo from teammate. Updated read me.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # i-need-a-rec
+
+#### Get recommendations for destination! Just tell us where you're going and what you want to do/see.
+
+* [Deployed](http://jshou403.github.io/i-need-a-rec)
+* [Repository](https://github.com/jshou403/i-need-a-rec)
+
+## How The App Works
+
+"I Need A Rec" provides recommendations for the location of the user's choice and search categories! Using Yelp, the user can search up to three categories of their choice and view the top 5 results for each category. If the user wants to see a recommendation on the map, they have the option to "map it." The user also has the option to pin their hotel or location of choice to the same map by adding in their address to view proximity and plan accordingly. 
+
+## Future Developments
+* Allow user to save pinned locations to an itinerary. 
+
+## Technologies Used
+* Javascript
+* JQuery
+* HTML/CSS 
+* Materialize CSS Framework
+* Yelp API
+* Google Maps API
+
+## Developers
+Jacalyn Shou - Project Manager, Front End Developer
+* [Portfolio](http://www.jacalynshou.com/)
+* [Github](https://github.com/jshou403)
+* [LinkedIn](https://www.linkedin.com/in/jacalyn-shou/)
+
+Leah Kurtz - Back End Developer
+* [Portfolio](https://lfkurtz7.github.io/)
+
+Ajay Kiri - Back End Developer
+* [Portfolio](https://sahasra101.github.io/Responsive-Portfolio/)
---
+++ assets/javascript/app.js
@@ -0,0 +1,194 @@
+// YELP 
+
+function yelp(city, category = "Dining", yelpDisplay = "#yelp-display-1") {
+
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + category + "&location=" + city
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        headers: {
+            Authorization: "Bearer f_OWbSivdwPfDyl30viNm1pnNTDjKRkxJILe56UtQARE0RCvNNOoDY11kZPHD3XBxpDDsHLcQ4gUlUfE_-CxM89He8J5bEgGByTO56_pk-LILybh25BE7NgrRLQsXXYx",
+        },
+        dataType: 'json',
+    })
+
+        .then(function (response) {
+
+            // console.log("Ajax Response below");
+            // console.log(response);
+
+            $(yelpDisplay).empty();
+
+            // var results = response.data;
+            // console.log(results);
+
+            for (var j = 0; j < 5; j++) {
+
+                var yelpResult = $("<div>");
+                yelpResult.attr("class", "card-panel hoverable");
+
+                var bName = response.businesses[j].name;
+                var bImage = response.businesses[j].image_url;
+                var bRating = response.businesses[j].rating;
+                var bPhone = response.businesses[j].phone;
+                var bLocation = response.businesses[j].location.display_address;
+                var lat = response.businesses[j].coordinates.latitude;
+                var lng = response.businesses[j].coordinates.longitude;
+                var myLatlng = { lat, lng };
+                var addBtn = $("<button>").addClass("btn btn-small add-to-map-btn").text("Map It");
+                addBtn.attr("data-lng", lng);
+                addBtn.attr("data-lat", lat);
+
+                // console.log("Business Name = " + bName);
+                // console.log("Image URL = " + bImage);
+                // console.log("Phone = " + bPhone);
+                // console.log("Address = " + bLocation);
+                // console.log("Rating = " + bRating);
+                // console.log("Latitude, longitute = " + lat, lng);
+                // console.log(myLatlng);
+
+                var imageRender = $("<img>")
+                imageRender.attr("src", bImage);
+                imageRender.attr("class", "responsive-img", bImage);
+                yelpResult.append(imageRender);
+                yelpResult.append("<b>" + bName + "</b>");
+                yelpResult.append("</br><i class=\"fas fa-star\"></i> " + bRating + " Stars");
+                yelpResult.append("</br><i class=\"fas fa-map-marker-alt\"></i> " + bLocation);
+                yelpResult.append("</br><i class=\"fas fa-phone-alt\"></i> " + bPhone);
+                yelpResult.append("</br>");
+                yelpResult.append(addBtn);
+                $(yelpDisplay).append(yelpResult);
+                // console.log(yelpResult)
+            }
+
+        })
+}
+
+// On page load, 
+// display top 5 search results for 
+// 3 pre-selected categories for 1 pre-selected city
+ 
+$(document).ready(function () {
+
+    // console.log("Page loads.");
+
+    var cityName = $(this).val() || "Amsterdam";
+    var category1 = $("#yelp-search-1").val() || "Restaurants";
+    var category2 = $("#yelp-search-2").val() || "Best Museums";
+    var category3 = $("#yelp-search-3").val() || "Gardens";
+
+    yelp(cityName, category1, "#yelp-display-1");
+    yelp(cityName, category2, "#yelp-display-2");
+    yelp(cityName, category3, "#yelp-display-3")
+
+    $("#city-name").on("keyup", function (event) {
+        event.preventDefault();
+        cityName = $(this).val();
+
+        yelp(cityName, category1, "#yelp-display-1");
+        yelp(cityName, category2, "#yelp-display-2");
+        yelp(cityName, category3, "#yelp-display-3");
+        console.log(cityName);
+    })
+
+    $("#yelp-search-1").on("keyup", function (event) {
+        event.preventDefault();
+        console.log("This is yelp search 1");
+
+        category1 = $("#yelp-search-1").val();
+
+        yelp(cityName, category1, "#yelp-display-1");
+        console.log(category1);
+        // console.log("city1 " + cityName)
+    })
+
+    $("#yelp-search-2").on("keyup", function (event) {
+        event.preventDefault();
+        console.log("This is yelp search 2");
+        category2 = $("#yelp-search-2").val()
+
+        yelp(cityName, category2, "#yelp-display-2");
+        console.log(category2)
+        //console.log("city2 " + cityName)
+    })
+
+    $("#yelp-search-3").on("keyup", function (event) {
+        event.preventDefault();
+        category3 = $("#yelp-search-3").val()
+
+        yelp(cityName, category3, "#yelp-display-3");
+        console.log(category3)
+        //console.log("city3 " + cityName)
+    })
+});
+
+// GOOGLE MAPS
+
+var map;
+var geocoder;
+var markers = [];
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: { lat: 52.366, lng: 4.895 }
+    });
+    geocoder = new google.maps.Geocoder();
+
+    document.getElementById('submit').addEventListener('click', function () {
+        geocodeAddress(geocoder, map);
+    });
+}
+
+$(document).on("click", ".add-to-map-btn", function () {
+    // deleteMarkers();
+    var lat = parseFloat($(this).attr("data-lat"));
+    var lng = parseFloat($(this).attr("data-lng"));
+    map.setCenter({ lat, lng });
+    console.log({ lat, lng });
+    var marker = new google.maps.Marker({
+        map: map,
+        position: { lat, lng }
+    });
+    markers.push(marker);
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#map").offset().top
+    }, 1000);
+});
+
+function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            console.log(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+        markers.push(marker);
+    });
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+function clearMarkers() {
+    setMapOnAll(null);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
